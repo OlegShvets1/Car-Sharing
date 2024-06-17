6bcf0a7c3551b46df2a5f54f89dab5c23904c52c
Oleh Shvets <olehpost69@gmail.com>
2024-06-17 21:56:09 +0300

    thirth_commit3

---
+++ src/test/resources/database/cars/add-rentals-to-rentals-table.sql
@@ -0,0 +1,4 @@
+INSERT INTO rentals (id, rental_date, required_return_date, actual_return_date, car_id, user_id, is_active, is_deleted) VALUES
+(1, '2024-06-01', '2024-06-10', NULL, 1, 1, TRUE, FALSE),
+(2, '2024-06-01', '2024-06-10', NULL, 1, 2, TRUE, FALSE),
+(3, '2024-06-01', '2024-06-10', '2024-06-10', 2, 2, TRUE, FALSE);
